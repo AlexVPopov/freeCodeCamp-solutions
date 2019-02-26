595b4a9c4eabbf4f80c374a707e758bfbf5e46fb
Alexander Popov <alex.val.popov@gmail.com>
2019-02-26 18:26:24 +0200

    Refactor solution to Smallest common multiple

--- Javascript Algorithms And Data Structures/Intermediate Algorithm Scripting/14_smallest_common_multiple.js
+++ Javascript Algorithms And Data Structures/Intermediate Algorithm Scripting/14_smallest_common_multiple.js
@@ -1,68 +1,23 @@
-// https://learn.freecodecamp.org/javascript-algorithms-and-data-structures/intermediate-algorithm-scripting/smallest-common-multiple
-
-function smallestCommons(arr) {
-  let [low, high] = arr.sort((first, second) => { return first - second })
-
-  let range = Array.from(Array(high + 1).keys()).slice(low).filter(number => number > 1)
-
-  let primeFactorizations = range.map(number => primeFactorize(number))
-
-  let primeFactors = new Set(primeFactorizations.flat())
-
-  let emptyExponents = [...primeFactors].reduce((accumulator, prime) => {
-    accumulator[prime] = 0
-    return accumulator
-  }, {})
-
-  let exponents = primeFactorizations.map(primeFactorization => {
-    return primeFactorization.reduce((accumulator, prime) => {
-      accumulator[prime] += 1
-      return accumulator
-    }, JSON.parse(JSON.stringify(emptyExponents)))
-  })
-
-  let result = {}
-
-  for (let prime of primeFactors) {
-    result[prime] = 0
-    exponents.reduce((acc, exponent) => {
-      if (acc[prime] < exponent[prime]) { acc[prime] = exponent[prime] }
-
-      return acc
-    }, result)
-  }
-
-  let sum = Object.keys(result).reduce((currentSum, key) => {
-    currentSum *= Math.pow(parseInt(key), result[key])
-    return currentSum
-  }, 1)
-
-  return sum
+function gcd(arr) {
+  let [x, y] = arr.sort((a, b) => { return b - a })
+  return y == 0 ? x : gcd([y, x % y])
 }
 
-function primeFactorize(num) {
-  if (num < 2) { return [] }
+function lcm(arr) {
+  return arr[0] * arr[1] / gcd(arr)
+}
 
-  if (num === 2) { return [2] }
+function smallestCommons(arr) {
+  arr = arr.sort((a, b) => { return b - a })
 
-  let sieve = []
-  let primeFactors = []
+  let range = []
 
-  for (let i = 2; i <= num; i++) {
-
-    if (!sieve[i]) {
-      while (num % i == 0) {
-        primeFactors.push(i)
-        num /= i
-      }
-
-      for (let j = i; j <= num; j += i) {
-        sieve[j] = true
-      }
-    }
+  for (let i = arr[0]; i >= arr[1]; i--) {
+    range.push(i)
   }
 
-  return primeFactors
+  return range.reduce((currentLcm, element) => { return lcm([currentLcm, element]) })
 }
 
 console.log(smallestCommons([23, 18]))
+
